4dffa7dad937f247c40ea067741a2e12702b091d
angel <angelsmithlgs@gmail.com>
2024-08-27 16:59:14 -0400

    build: structure SoapUi

---
+++ src/main/java/com/example/SoapService.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.xml.ws.Endpoint;
+
+@WebService
+public class SoapService {
+
+    @WebMethod
+    public String sayHello(String name) {
+        return "Hello, " + name + "!";
+    }
+
+    public static void main(String[] args) {
+        Endpoint.publish("http://localhost:8080/SoapService", new SoapService());
+        System.out.println("Service is running at http://localhost:8080/SoapService?wsdl");
+    }
+}
